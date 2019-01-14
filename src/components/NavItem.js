1549ac241ccbfc24a58af26b374542e493a53b68
Esli Alejandro Resendez Guerrero <WVM683@motorolasolutions.com>
2019-01-14 12:04:44 -0600

    Add GitIgnore File

--- src/components/NavItem.js
+++ src/components/NavItem.js
@@ -5,6 +5,8 @@ import {Route, Link} from 'react-router-dom'
 export const NavItem = ({children, to, exact}) => {
     var classname_active = "nav-item nav-link active";
     var classname_inactive = "nav-item nav-link inactive"
+    var link_classname_active = "badge badge-dark";
+    var link_classname_inactive = "badge badge-light";
 //
 //    if (exact){
 //        classname_str = "nav-item nav-link active";
@@ -13,7 +15,7 @@ export const NavItem = ({children, to, exact}) => {
     return (
         <Route path={to} exact={exact} children={({match}) => (
             <li className={match ? classname_active : classname_inactive}>
-                <Link to={to}>{children}</Link>
+                <Link to={to} ><div className ={match ? link_classname_active : link_classname_inactive} >{children}</div> </Link>
             </li>
         )}/>
     )
